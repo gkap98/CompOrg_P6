4cd460259ee2aa3aa42537ccd443079eed6a69c1
Gavin Kaepernick <kaepernick.gavin6@gmail.com>
2019-04-25 09:56:50 -0500

    Added in .h Files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "types.h"
+#include "fs.h"
+#include "stat.h"
+
 using namespace std;
 
 int main() {
